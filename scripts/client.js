d70452f1a266f31ee9f8fa316e737ac1525b5831
Tobi-Tobar <72173550+Tobi-Tobar@users.noreply.github.com>
2022-10-31 21:32:55 -0700

    Cube time

--- scripts/client.js
+++ scripts/client.js
@@ -1,6 +1,6 @@
 // Art 109 Three.js Demo Site
 // client.js
-// A basic three.js scene which displays and rotates a polygon with a wireframe
+// A basic three.js scene which displays and rotates a cubegon with a wireframe
 
 // Extract globals from external script
 const { THREE } = window;
@@ -14,19 +14,19 @@ const camera = new THREE.PerspectiveCamera(
   1000
 );
 const renderer = new THREE.WebGLRenderer();
-renderer.setClearColor(0xdfdfdf);
+renderer.setClearColor(0x000000);
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(500, 500);
 document.body.appendChild(renderer.domElement);
 
 // Add a polygon to the scene
-const geometry = new THREE.IcosahedronGeometry(1, 0);
-const material = new THREE.MeshStandardMaterial({ color: 0x2e8e39 });
-const poly = new THREE.Mesh(geometry, material);
-scene.add(poly);
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const material = new THREE.MeshBasicMaterial( {color: 0xffffff} );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
 
 // add wireframe to shape
-const matLineBasic = new THREE.LineBasicMaterial({ color: 0x2e8e99 });
+const matLineBasic = new THREE.LineBasicMaterial({ color: 0xaca7d6 });
 const wireframe = new THREE.WireframeGeometry(geometry);
 const line = new THREE.LineSegments(wireframe, matLineBasic);
 line.material.depthTest = false;
@@ -51,8 +51,8 @@ function render() {
   requestAnimationFrame(render);
 
   // Rotate our shape
-  poly.rotation.x += 0.005;
-  poly.rotation.y += 0.005;
+  cube.rotation.x += 0.005;
+  cube.rotation.y += 0.005;
   line.rotation.x += 0.005;
   line.rotation.y += 0.005;
   renderer.render(scene, camera);
