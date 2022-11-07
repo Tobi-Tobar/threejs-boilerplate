d9c3200e3a995ef3db75af21a5cfea4ddb22fc03
Tobi-Tobar <bianka.tobar@sjsu.edu>
2022-11-07 14:22:09 -0800

    cow added

--- scripts/client2.js
+++ scripts/client2.js
@@ -37,7 +37,7 @@ var mesh;
 
 // Load GLTF model, add material, and add it to the scene
 const loader = new GLTFLoader().load(
-  "../../assets/testing123fogv3.glb", // comment this line out and un comment the line below to swithc models
+  "../../assets/cow.glb", // comment this line out and un comment the line below to swithc models
   //"./assets/gourd_web.glb", //<-- photogrammetery model
   function(gltf) {
     // Scan loaded model for mesh and apply defined material if mesh is present
